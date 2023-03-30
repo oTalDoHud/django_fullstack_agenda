9b9a828f3f301b675c6fa56b24f4df3e4f38bf31
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-03-30 17:13:19 -0300

    exibindo único contato

--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Contato
 
+
 def index(request):
     return render(request, "contatos/index.html", {
         "contatos": Contato.objects.all()
+    })
+
+
+def ver_contato(request, contato_id):
+    return render(request, "contatos/ver_contato.html", {
+        "contato": Contato.objects.get(id=contato_id)
     })
