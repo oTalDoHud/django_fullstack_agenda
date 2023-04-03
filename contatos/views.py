28b4caaec7c7755eb9e906bc4ca4eb87fc8ad3d6
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-04-03 10:23:15 -0300

    Order e Filter

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by("-id").filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 1)
 
     page = request.GET.get("page")
@@ -16,6 +19,11 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, "contatos/ver_contato.html", {
-        "contato": get_object_or_404(Contato, id=contato_id)
+        "contato": contato
     })
