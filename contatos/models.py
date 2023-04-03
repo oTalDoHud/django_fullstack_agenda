059a2697158dca85bb711f87c2392a2e87b7ad44
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-04-03 12:05:39 -0300

    Adicionando imagens

--- contatos/models.py
+++ contatos/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -16,10 +18,7 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to="fotos/%Y/%m/")
 
     def __str__(self) -> str:
         return self.nome
-
-
-
-
